3aad2660b00fbf927698c6e6921cc3759705ae82
TRUONG Huu-Tien <truong.htien@gmail.com>
2014-01-30 13:51:42 -0500

    0.0.4 - initial Home

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@
 /*
  * GET home page.
  */
+var app = {
+	title	: "Sengami",
+	version	: "0.0.3"	
+};
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Sengami', version: "0.0.3" });
+  res.render('index', { title: app.title, version: app.version });
+};
+exports.home = function(req, res){
+  res.render('home', { title: app.title, version: app.version });
 };
